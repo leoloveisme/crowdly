84ba6cf5c3154c1432054c1735c87b781361eac2
leotheforth <lzh_lad@gmx.de>
2022-02-04 18:29:42 +0100

    created crowdly today 4 February 2022

---
+++ home.php
@@ -0,0 +1,56 @@
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <style></style>
+  </head>
+  <body>
+  
+  <?php include ("modules/header"); ?>
+  
+  <table width=100%>
+  <tr>
+
+  <!-- left side menu -->
+
+  <td width=20%>
+  
+  
+  </td>
+
+  <!-- end of left side menu -->
+  
+  
+   <!-- central part --> 
+  
+  <td valign=top width=60%>
+  
+  <strong>Welcome</strong>, username
+  
+  <br><br>
+  <input type="text"> &nbsp; <input type="button" value="Search">
+    <br><br>
+  <input type="button" value="Start new project">
+  
+  </td>
+  
+    <!-- end of the central part -->
+    
+  
+  <!-- right side menu -->
+  
+<td valign=top width=20%>
+
+
+
+</td> 
+
+  <!-- end of right side menu -->
+
+  </tr>
+  </table>
+
+<?php include ("modules/footer"); ?>
+
+  </body>
+</html>
---
+++ start.php
@@ -0,0 +1,47 @@
+<html>
+  <head>
+    <title></title>
+    <meta content="">
+    <style></style>
+  </head>
+  <body>
+  <table width=100%>
+  <tr>
+  <td width=20%></td>
+  
+  <td valign=top width=60% align=center>
+
+<table>
+<tr>
+<td>
+ username / email 
+ </td>
+ <td>
+  <input type="text">
+ </td>
+ </tr>
+  <tr>
+  <td>
+  password (show)
+  </td> 
+  <td>
+  <input type="password">
+  </td>
+  </tr>
+<tr>
+<td colspan=2 align=right>
+<input type="button" value="Clear"> &nbsp; <input type="button" value="Go">
+</td>
+</tr>
+</table>
+
+</td>
+  
+<td valign=top width=20%>
+
+</td> 
+
+  </tr>
+  </table>
+  </body>
+</html>
